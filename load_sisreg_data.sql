99280c167b467487b683dcd5068d3b84350656b7
YagoRosa <30865549+YagoRosa@users.noreply.github.com>
2025-11-11 20:53:35 -0300

    Fix: ajustes no BULK INSERT e tabela
    💡 Observações técnicas:
    Durante o processo de ingestão, surgiram erros relacionados ao formato original do arquivo exportado do SISREG. Foi necessário ajustar os parâmetros do BULK INSERT para lidar com inconsistências de estrutura e codificação, garantindo que os dados fossem carregados corretamente no SQL Server.
    
    Foram aplicados os seguintes ajustes:
    
    - FIELDQUOTE para tratar campos delimitados por aspas duplas;
    - TABLOCK para otimizar o carregamento em massa;
    - TRUNCATE TABLE antes da carga para evitar duplicidade de registros;
    - ERRORFILE para registrar as linhas com erro e facilitar a análise dos dados problemáticos.
    
    Essas configurações garantem que o pipeline leia corretamente os dados CSV e mantenha a integridade da tabela destino.
    
    Os registros gerados pelo parâmetro ERRORFILE foram fundamentais para diagnosticar as causas dos erros de carga. Dada a importância desse tipo de log no processo de ingestão, serão desenvolvidas rotinas de tratamento automatizado de erros, permitindo identificar e corrigir inconsistências de forma mais ágil, prevenindo falhas e otimizando a confiabilidade do pipeline em futuras execuções.

--- load_sisreg_data.sql
+++ load_sisreg_data.sql
@@ -1,11 +1,21 @@
 USE MeuBancoTeste;
 
-BULK INSERT Solicitacoes 
-FROM 'caminho aqui'
-WITH (
-    FIELDTERMINATOR = ',',
-    ROWTERMINATOR = '\r\n',
-    FIRSTROW = 2,
-    CODEPAGE = '65001'
+TRUNCATE TABLE Solicitacoes;
 
+
+BULK INSERT Dados 
+FROM 'C:\Users\yagos\Documents\Projetos\sisregPipeline\SISREG.csv'
+WITH (
+    FORMAT = 'CSV',
+    FIELDTERMINATOR = ';',
+    FIELDQUOTE = '"',  -- Importante: campos entre aspas
+    ROWTERMINATOR = '\n',
+    FIRSTROW = 2,
+   
+    ERRORFILE = 'C:\Users\yagos\Documents\Projetos\sisregPipeline\erros',
+    MAXERRORS = 1000,
+    TABLOCK
 );
+
+
+
